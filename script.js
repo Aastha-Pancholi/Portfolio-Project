c77f47c9825cac10513ada7f4125764a9e17e7df
Aastha Pancholi <aasthadp2002@gmail.com>
2024-06-11 11:06:48 +0530

    added-sidebar-close

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const open1 = document.querySelector("#toggle-btn");
 open1.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+const sidebar = document.querySelector("#sidebar");
+
+document.onclick=function(e){
+    if(!sidebar.contains(e.target)){
+        document.querySelector("#sidebar").classList.remove("expand");
+    }
+}
